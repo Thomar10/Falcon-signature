b0ccc492514e3ed11590de4da16c75a0cca5807f
Thomas Luxhøj <tlux-8@hotmail.com>
2023-02-16 17:04:26 +0100

    Implementing falcon

--- falcon/src/falcon.rs
+++ falcon/src/falcon.rs
@@ -1,4 +1,6 @@
-use crate::shake::InnerShake256Context;
+use std::ptr::{null, null_mut};
+use crate::keygen::keygen;
+use crate::shake::{i_shake256_extract, i_shake256_flip, i_shake256_init, i_shake256_inject, InnerShake256Context};
 #[macro_export]
 macro_rules! falcon_tmpsize_keygen {
     ($arg:expr) => {
@@ -47,9 +49,67 @@ macro_rules! falcon_tmpsize_verify {
     ($arg:expr) => {(8u << $arg) + 1}
 }
 
+
+pub fn shake256_init(rng: &mut InnerShake256Context) {
+    i_shake256_init(rng);
+}
+
+pub fn shake256_inject(rng: &mut InnerShake256Context, data: &mut [u8]) {
+    i_shake256_inject(rng, data);
+}
+
+pub fn shake256_flip(rng: &mut InnerShake256Context) {
+    i_shake256_flip(rng);
+}
+
+// TODO EXTRACT MUTATE INSTEAD?
+pub fn shake256_extract(rng: &mut InnerShake256Context, out: &mut [u], len: usize) {
+    i_shake256_extract(rng, len);
+}
+
+pub fn shake_init_prng_from_seed(rng: &mut InnerShake256Context, seed: &mut [u], _len: usize) {
+    i_shake256_init(rng);
+    shake256_inject(rng, seed);
+}
+
+pub fn shake_init_prng_from_system(rng: &mut InnerShake256Context) -> i32 {
+    let mut seed: [u8; 48] = [0; 48];
+    // TODO ? get_seed defined in rng for Extra/c/rng
+    // if !get_seed(seed, 48) {
+    //     return -1;
+    // }
+    i_shake256_init(rng);
+    shake256_inject(rng, &mut seed);
+    0
+}
+
+// see inner.h for set_fpu_cw for better / more correct implementation.
+pub(crate) fn set_fpu_cw(x: u32) -> u32 {
+    x
+}
+
 pub fn falcon_keygen_make(rng: &mut InnerShake256Context, logn: u32, private_key: &mut [u8],
                           private_len: usize, public_key: &mut [u8], public_len: usize,
                           tmp: &mut [u8], tmp_len: usize) -> i32 {
+    if logn < 1 || logn > 10 {
+        return -5;
+    }
+    if private_len < falcon_privatekey_size!(logn)
+        || (public_key.len() <= 0 && public_len < falcon_publickey_size!(logn))
+        || tmp_len < falcon_tmpsize_keygen!(logn) {
+        return -2;
+    }
+    let n = 1usize << logn;
+    let f: *mut u8 = tmp.as_mut_ptr();
+    let g = f.wrapping_add(n);
+    let F = g.wrapping_add(n);
+    // TODO alignment problems maybe?
+    let atmp: *mut u8 = F.wrapping_add(n);
+    let oldcw = set_fpu_cw(2);
+    keygen(rng, f.cast(), g.cast(), F.cast(), null_mut(), null_mut(), logn, atmp);
+
+    // Encode
+
     0
 }
 
