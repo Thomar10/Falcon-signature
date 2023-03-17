196d3dfd34a1ef0e54645a172952392cc3b00da0
Malthe Glent-Madsen <malthe08@hotmail.com>
2023-03-17 09:30:19 +0100

    Like writing a text message in a dream

--- chipwhisperer/src/bin/unmasked.rs
+++ chipwhisperer/src/bin/unmasked.rs
@@ -1,21 +1,39 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
+use bytemuck;
 use embedded_alloc::Heap;
 use cortex_m_rt::entry;
 use panic_halt as _;
 use stm32f3xx_hal::block;
+use stm32f3xx_hal::gpio::{Gpioa, Output, Pin, PushPull, U};
 use stm32f3xx_hal::prelude::*;
 use chipwhisperer::setup;
+use falcon::common::hash_to_point_vartime;
+use falcon::falcon::fpr;
+use falcon::{falcon_tmpsize_expandprivate, falcon_tmpsize_keygen};
 
 use falcon::keygen::keygen;
 use falcon::shake::{i_shake256_init, i_shake256_inject, InnerShake256Context};
+use falcon::sign::{expand_privkey, sign_tree};
 
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
 
 #[entry]
 fn main() -> ! {
+
+    //Initialize allocator
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+    }
+
+
     let (mut trigger, usb_serial) = setup();
     let (mut tx, mut rx) = usb_serial.split();
 
@@ -31,14 +49,23 @@ fn main() -> ! {
         let left: u32 = u32::from_le_bytes([read_buffer[0], read_buffer[1], read_buffer[2], read_buffer[3]]);
         let right: u32 = u32::from_le_bytes([read_buffer[4], read_buffer[5], read_buffer[6], read_buffer[7]]);
 
-        trigger.set_high().unwrap();
-        let result = rust_test(left, right);
-        trigger.set_low().unwrap();
+        rust_genkey(&mut trigger);
 
-        let mut write_buffer: [u8; 4] = u32::to_le_bytes(result);
+        /*let mut result: u32 = 0;
+        cortex_m::interrupt::free(|_| {
+            trigger.set_high().unwrap();
+            result = rust_test(3, 42);
+            //result = rust_genkey(&mut trigger);
+            trigger.set_low().unwrap();
+        });*/
+
+        //result = rust_genkey(&mut trigger);
+        //result = 120;
+        let mut write_buffer: [u8; 4] = [0; 4]; //u32::to_le_bytes(result);
         let wlen: usize = 4;
+        //block!(tx.write(32u8));
         for i in 0..wlen {
-            block!(tx.write(write_buffer[i]));
+            let write_res = block!(tx.write(write_buffer[i]));
         }
     }
 }
@@ -47,7 +74,7 @@ fn rust_test(left: u32, right: u32) -> u32 {
     let mut result: u32 = 0;
     //let mut h: [u16; 1024] = [0; 1024];
 
-    for _ in 0..50 {
+    for _ in 0..10 {
         result += left + right;
         //h[0] = result as u16;
     }
@@ -55,12 +82,34 @@ fn rust_test(left: u32, right: u32) -> u32 {
     return result;
 }
 
-fn rust_genkey() -> u32{
+fn rust_genkey(trigger: &mut  Pin<Gpioa, U<12>, Output<PushPull>>){
 
     let input: [u8; 6] = [1, 2, 3, 4, 5, 6];
 
+    const LOGN: usize = 5;
+    const BUFFER_SIZE: usize = 272 + ((3 << LOGN) + 7);
+    let mut h: [u16; 1024] = [0; 1024];
+    let mut f: [i8; 1024] = [0; 1024];
+    let mut g: [i8; 1024] = [0; 1024];
+    let mut F: [i8; 1024] = [0; 1024];
+    let mut G: [i8; 1024] = [0; 1024];
+
+    let mut tmp_keygen: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+    let mut rng_rust: InnerShake256Context = gen_rng(&input);
+    cortex_m::interrupt::free(|_| {
+        trigger.set_high().unwrap();
+        keygen(&mut rng_rust, &mut f, &mut g, &mut F, &mut G, &mut h, LOGN as u32, &mut tmp_keygen);
+        trigger.set_low().unwrap();
+    });
+    return;
+}
+
+/*fn rust_sign(trigger: &mut  Pin<Gpioa, U<12>, Output<PushPull>>) -> u32 {
+    let input: [u8; 6] = [1, 2, 3, 4, 5, 6];
+
     const LOGN: usize = 10;
-    const BUFFER_SIZE: usize = 8192 * 8;
+    const N: usize = 1 << LOGN;
+    const BUFFER_SIZE: usize = falcon_tmpsize_keygen!(LOGN);
     let mut h: [u16; 1024] = [0; 1024];
     let mut f: [i8; 1024] = [0; 1024];
     let mut g: [i8; 1024] = [0; 1024];
@@ -70,8 +119,22 @@ fn rust_genkey() -> u32{
     let mut tmp_keygen: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
     let mut rng_rust: InnerShake256Context = gen_rng(&input);
     keygen(&mut rng_rust, &mut f, &mut g, &mut F, &mut G, &mut h, LOGN as u32, &mut tmp_keygen);
-    return h[0] as u32;
-}
+    const EXKLENGTH: usize = (LOGN + 40) << LOGN;
+    let mut expanded_key: [fpr; EXKLENGTH] = [0; EXKLENGTH];
+    let mut tmp_key: [fpr; falcon_tmpsize_expandprivate!(LOGN)] = [0; falcon_tmpsize_expandprivate!(LOGN)];
+    expand_privkey(&mut expanded_key, &mut f, &mut g, &mut F, &mut G, LOGN as u32, &mut tmp_key);
+    let mut sig: [i16; 1024] = [0; 1024];
+
+    let mut sc = gen_rng(&input);
+    let mut hm: [u16; 1024] = [0; 1024];
+    hash_to_point_vartime(&mut sc, &mut hm, LOGN as u32);
+
+    trigger.set_high().unwrap();
+    sign_tree(&mut sig, &mut rng_rust, &mut expanded_key, &hm, LOGN as u32, &mut tmp_keygen);
+    trigger.set_low().unwrap();
+
+    return sig[0] as u32;
+}*/
 
 fn gen_rng(input: &[u8]) -> InnerShake256Context {
     let state: [u64; 25] = [0; 25];
--- chipwhisperer/trace.py
+++ chipwhisperer/trace.py
@@ -3,6 +3,7 @@ import chipwhisperer as cw
 import os
 from chipwhisperer.capture.api.programmers import STM32FProgrammer
 import matplotlib.pyplot as plt
+import time
 
 try:
     if not scope.connectStatus:
@@ -11,15 +12,22 @@ except NameError:
     scope = cw.scope()
 
 scope.default_setup()
-target_type = cw.targets.SimpleSerial2
-target = cw.target(scope, target_type)
+#target_type = cw.targets.SimpleSerial2
+
+try:
+    target = cw.target(scope)
+except:
+    print("INFO: Caught exception on reconnecting to target - attempting to reconnect to scope first.")
+    print("INFO: This is a work-around when USB has died without Python knowing. Ignore errors above this line.")
+    scope = cw.scope()
+    target = cw.target(scope)
 
 print("INFO: Found ChipWhisperer")
 
 program = STM32FProgrammer
 
 dir = os.path.dirname(os.path.realpath(__file__))
-program_hex_path = os.path.join(dir, r"simpleserial-target-CW308_STM32F3.hex") #Update accordingly
+program_hex_path = os.path.join(dir, r"unmasked.hex") #Update accordingly
 
 cw.program_target(scope, program, program_hex_path)
 
@@ -27,22 +35,29 @@ scope.arm()
 target.flush()
 
 # Dummy data
-data = bytearray([0x42] * 16)
+data = bytearray([0, 8, 1, 2, 3, 4, 5, 6, 7, 8])
 
 # Send command to trigger code execution
 #Trigger test
 #target.send_cmd('t', 0x80, data)
 
-target.send_cmd('g', 0x80, data)
+target.write(data)
+
+""" print("Waiting 10 seconds for board to do computation:")
+for i in range(10):
+    time.sleep(1)
+    print("", str(9 - i)) """
 
 # Fetch trace
 ret = scope.capture()
 trace = scope.get_last_trace()
 
 # Print the returned data
-returned_data = target.read_cmd('r')
-print(returned_data)
-ack = target.read_cmd('e')
+#cmd = target.read(1)
+returned_data = target.read()
+returned_val = int.from_bytes(bytes(returned_data, 'latin1'), "little")
+print("Bytes:", list(bytes(returned_data, 'latin1')))
+print("Data:", returned_val)
 
 # Plot the trace
 plt.plot(trace)
