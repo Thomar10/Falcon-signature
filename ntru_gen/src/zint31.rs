3ac8dbae5541d9598bbdc0d113402e328808af00
Thomas Luxhøj <tlux-8@hotmail.com>
2023-03-19 16:57:51 +0100

    rebuild done

--- ntru_gen/src/zint31.rs
+++ ntru_gen/src/zint31.rs
@@ -1,4 +1,4 @@
-use crate::mp31::{mp_add, mp_half, mp_montymul, tbmask};
+use crate::mp31::{mp_add, mp_half, mp_montymul, mp_sub, PRIMES, tbmask};
 
 pub fn zint_mul_small(m: &mut [u32], len: usize, x: u32) -> u32 {
     let mut cc: u32 = 0;
@@ -16,8 +16,8 @@ pub fn zint_mod_small_unsigned(d: &[u32], len: usize, stride: usize, p: u32, p0i
     let mut d_index = len * stride;
     for _ in (0..len).rev() {
         d_index -= stride;
-        let mut w: u32 = d[d_index] - p;
-        w += p & tbmask(w);
+        let mut w: u32 = d[d_index].wrapping_sub(p);
+        w = w.wrapping_add(p & tbmask(w));
         x = mp_montymul(x, z, p, p0i);
         x = mp_add(x, w, p);
     }
@@ -36,4 +36,70 @@ pub fn zint_add_mul_small(x: &mut [u32], len: usize, xstride: usize, y: &[u32], 
         x_index += xstride;
     }
     x[x_index] = cc;
+}
+
+pub fn zint_norm_zero(x: &mut [u32], len: usize, xstride: usize, p: &[u32]) {
+    let mut r: u32 = 0;
+    let mut bb: u32 = 0;
+    let mut x_index = len * xstride;
+    for u in (0..len).rev() {
+        x_index -= xstride;
+        let wx = x[x_index];
+        let wp = (p[u] >> 1) | (bb << 30);
+        bb = p[u] & 1;
+
+        let mut cc: u32 = wp.wrapping_sub(wx);
+        cc = ((!cc).wrapping_add(1) >> 31) | (!(cc >> 31)).wrapping_add(1);
+
+        r |= cc & ((r & 1).wrapping_sub(1));
+    }
+
+    let mut cc: u32 = 0;
+    let m = tbmask(r);
+    for j in 0..len {
+        let mut xw = x[x_index];
+        let w = xw.wrapping_sub(p[j]).wrapping_sub(cc);
+        cc = w >> 31;
+        xw ^= ((w & 0x7FFFFFFF) ^ xw) & m;
+        x[x_index] = xw;
+        x_index += xstride;
+    }
+}
+
+pub fn zint_rebuild_crt(xx: &mut [u32], xlen: usize, n: usize, num_sets: usize, normalized_signed: bool, tmp: &mut [u32]) {
+    let mut uu = 0;
+    tmp[0] = PRIMES[0].p;
+    for u in 1..xlen {
+        let (p, p0i, s, r2): (u32, u32, u32, u32);
+
+        p = PRIMES[u].p;
+        p0i = PRIMES[u].p0i;
+        r2 = PRIMES[u].r2;
+        s = PRIMES[u].s;
+        uu += n;
+        let mut kk = 0;
+        for _ in 0..num_sets {
+            for v in 0..n {
+                let xp = xx[kk + v + uu];
+                let x_off = xx.split_at_mut(kk + v).1;
+                let xq = zint_mod_small_unsigned(x_off, u, n, p, p0i, r2);
+                let xr = mp_montymul(s, mp_sub(xp, xq, p), p, p0i);
+
+                zint_add_mul_small(xx.split_at_mut(kk + v).1, u, n, tmp, xr);
+            }
+            kk += n * xlen;
+        }
+        tmp[u] = zint_mul_small(tmp, u, p);
+    }
+
+    if normalized_signed {
+        let mut kk = 0;
+        for _ in 0..num_sets {
+            for v in 0..n {
+                let (_, x) = xx.split_at_mut(kk + v);
+                zint_norm_zero(x, xlen, n, tmp);
+            }
+            kk += n * xlen;
+        }
+    }
 }
--- ntru_gen_test/src/zint31.rs
+++ ntru_gen_test/src/zint31.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
-    use ntru_gen::zint31::{zint_add_mul_small, zint_mod_small_unsigned, zint_mul_small};
-    use ntru_gen_c::zint31::{ntrugen_zint_add_mul_small, ntrugen_zint_mod_small_unsigned, ntrugen_zint_mul_small};
+    use ntru_gen::zint31::{zint_add_mul_small, zint_mod_small_unsigned, zint_mul_small, zint_norm_zero, zint_rebuild_crt};
+    use ntru_gen_c::zint31::{ntrugen_rebuild_CRT, ntrugen_zint_add_mul_small, ntrugen_zint_mod_small_unsigned, ntrugen_zint_mul_small, ntrugen_zint_norm_zero};
     use rand::Rng;
 
     const P: u32 = 12289;
@@ -25,7 +25,7 @@ mod tests {
         for _ in 0..100 {
             let p0i: u32 = rand::random();
             let r2: u32 = rand::random();
-            let stride = 4;
+            let stride = rng.gen_range(1..5);
             let mut m: [u32; 1024] = core::array::from_fn(|_| rng.gen::<u32>());
             let x = zint_mod_small_unsigned(&mut m, 1024 / stride, stride, P, p0i, r2);
             let xc = unsafe { ntrugen_zint_mod_small_unsigned(m.as_ptr(), 1024 / stride, stride, P, p0i, r2) };
@@ -38,13 +38,63 @@ mod tests {
         let mut rng = rand::thread_rng();
         for _ in 0..100 {
             let s: u32 = rand::random();
+            let stride: usize = rng.gen_range(0..10);
+            let mut x: [u32; 1048] = core::array::from_fn(|_| rng.gen::<u32>());
+            let xc: [u32; 1048] = x.clone();
+            let y: [u32; 1048] = core::array::from_fn(|_| rng.gen::<u32>());
+            zint_add_mul_small(&mut x, 100, stride, &y, s);
+            unsafe { ntrugen_zint_add_mul_small(xc.as_ptr(), 100, stride, y.as_ptr(), s) };
+            assert_eq!(x, xc);
+        }
+    }
+
+    #[test]
+    fn zint_norm_zero_test() {
+        let mut rng = rand::thread_rng();
+        for _ in 0..100 {
+            let s: u32 = rand::random();
             let stride: usize = 1;
             let mut x: [u32; 20] = core::array::from_fn(|_| rng.gen::<u32>());
             let xc: [u32; 20] = x.clone();
-            let y: [u32; 20] = core::array::from_fn(|_| rng.gen::<u32>());
-            zint_add_mul_small(&mut x, 19, stride, &y, s);
-            unsafe { ntrugen_zint_add_mul_small(xc.as_ptr(), 19, stride, y.as_ptr(), s) };
+            let p: [u32; 20] = core::array::from_fn(|_| rng.gen::<u32>());
+            zint_norm_zero(&mut x, 19, stride, &p);
+            unsafe { ntrugen_zint_norm_zero(xc.as_ptr(), 19, stride, p.as_ptr()) };
             assert_eq!(x, xc);
+        }
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_zint_rebuild_CRT() {
+        for _ in 0..200 {
+             for stride in 1usize..3 {
+                let mut rng = rand::thread_rng();
+                let mut xx: [u32; 2048] = [10; 2048];
+                let xxc: [u32; 2048] = [10; 2048];
+                let num: usize = rng.gen_range(0..10);
+                let len: usize = 100;
+                let mut tmp: [u32; 5120] = [0; 5120];
+                let tmp_c: [u32; 5120] = [0; 5120];
+                zint_rebuild_crt(&mut xx, len, stride, num, false, &mut tmp);
+                unsafe { ntrugen_rebuild_CRT(xxc.as_ptr(), len, stride, num, 0, tmp_c.as_ptr()) };
+                assert_eq!(tmp, tmp_c);
+                assert_eq!(xx, xxc);
+            }
+        }
+        for _ in 0..200 {
+            for stride in 1usize..3 {
+                let mut rng = rand::thread_rng();
+                let mut xx: [u32; 2048] = core::array::from_fn(|_| rng.gen::<u32>());
+                let xxc: [u32; 2048] = xx.clone();
+                let num: usize = rng.gen_range(0..10);
+                let len: usize = 100;
+                let mut tmp: [u32; 5120] = core::array::from_fn(|_| rng.gen::<u32>());
+                let tmp_c: [u32; 5120] = tmp.clone();
+                zint_rebuild_crt(&mut xx, len, stride, num, true, &mut tmp);
+                unsafe { ntrugen_rebuild_CRT(xxc.as_ptr(), len, stride, num, 1, tmp_c.as_ptr()) };
+                assert_eq!(tmp, tmp_c);
+                assert_eq!(xx, xxc);
+            }
         }
     }
 }
