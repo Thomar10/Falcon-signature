03d0984ad6898412bb3fe5bd0c6bd1c9c0f98867
Thomas <tlux-8@hotmail.com>
2023-04-02 15:58:13 +0200

    fft

--- falcon_masked_test/src/tests/fft_masked_deep_test.rs
+++ falcon_masked_test/src/tests/fft_masked_deep_test.rs
@@ -2,8 +2,8 @@ use float_cmp::approx_eq;
 use rand::{Rng, thread_rng};
 
 use falcon::falcon::fpr;
-use falcon::fft::{fpc_add, fpc_mul, fpc_sub};
-use falcon_masked::fft_masked_deep::{secure_fpc_add, secure_fpc_mul, secure_fpc_sub};
+use falcon::fft::{fft, fpc_add, fpc_mul, fpc_sub};
+use falcon_masked::fft_masked_deep::{secure_fft, secure_fpc_add, secure_fpc_mul, secure_fpc_sub};
 
 #[test]
 fn fpc_add_test() {
@@ -75,6 +75,68 @@ fn fpc_sub_test() {
         check_eq_fpr(expected_im, result_im[0] ^ result_im[1]);
     }
 }
+
+#[test]
+fn test_fft() {
+    const LOGN: u32 = 10;
+    const LENGTH: usize = 1 << LOGN;
+    for _ in 0..5 {
+        let (mut f, mut f_shares): ([fpr; LENGTH], [[fpr; 2]; LENGTH]) = create_random_mask_arr::<LENGTH>();
+
+        secure_fft(&mut f_shares, LOGN);
+        fft(&mut f, LOGN);
+
+        //Reconstruct
+        let f_reconstructed: [fpr; LENGTH] = reconstruct_arr(&f_shares);
+
+        check_fpr_arr_eq(&f_reconstructed, &f);
+    }
+}
+
+
+fn check_fpr_arr_eq(x: &[fpr], y: &[fpr]) {
+    for i in 0..x.len() {
+        assert!(approx_eq!(f64, fpr_to_double(x[i]), fpr_to_double(y[i]), epsilon = 0.00003), "The two arrays differ in position {}\nLeft: fpr: {}, double: {}\nRight: fpr: {}, double: {}", i, x[i], fpr_to_double(x[i]), y[i], fpr_to_double(y[i]));
+    }
+}
+
+
+fn create_random_mask() -> (fpr, [fpr; 2]) {
+    let mut arr: [fpr; 2] = [0; 2];
+    let val: fpr = create_random_fpr();
+    arr[1] = create_random_fpr();
+    arr[0] = val ^ arr[1];
+    return (val, arr);
+}
+
+fn create_random_mask_arr<const LENGTH: usize>() -> ([fpr; LENGTH], [[fpr; 2]; LENGTH]) {
+    let mut f_shares: [[fpr; 2]; LENGTH] = [[0; 2]; LENGTH];
+    let mut f: [fpr; LENGTH] = [0; LENGTH];
+
+    for i in 0..LENGTH {
+        let (val, masked_val) = create_random_mask();
+        f[i] = val;
+        f_shares[i] = masked_val;
+    }
+
+    return (f, f_shares);
+}
+
+fn reconstruct(shares: &[fpr]) -> fpr {
+    return shares[0] ^ shares[1];
+}
+
+fn reconstruct_arr<const LENGTH: usize>(share_arr: &[[fpr; 2]; LENGTH]) -> [fpr; LENGTH] {
+    let mut reconstructed: [fpr; LENGTH] = [0; LENGTH];
+
+    for i in 0..LENGTH {
+        let res = reconstruct(&share_arr[i]);
+        reconstructed[i] = res;
+    }
+
+    return reconstructed;
+}
+
 
 pub fn check_eq_fpr(x: fpr, y: fpr) {
     assert!(approx_eq!(f64, fpr_to_double(x), fpr_to_double(y), epsilon = 0.000000003));
